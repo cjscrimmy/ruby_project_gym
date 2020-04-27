f620f715f4e8d3aedf692920b4742c3d5a3da5d0
cjscrimmy <craigscrimgeour@yahoo.com>
2020-04-27 10:14:58 +0100

    add code to save info into the members table. tested with postico and console.rb file

---
+++ ruby_project_gym_booking_system/models/console.rb
@@ -0,0 +1,22 @@
+require_relative("member")
+require_relative("lesson")
+require_relative("booking")
+
+require("pry")
+
+member1 = Member.new({
+    'first_name' => 'Steve',
+    'last_name' => 'Jones',
+    'date_of_birth' => '30/04/75'
+})
+member1.save()
+
+member2 = Member.new({
+    'first_name' => 'Karen',
+    'last_name' => 'Maxwell',
+    'date_of_birth' => '15/08/84'
+})
+member2.save()
+
+binding.pry
+nil
--- ruby_project_gym_booking_system/models/member.rb
+++ ruby_project_gym_booking_system/models/member.rb
@@ -4,13 +4,30 @@ class Member
     attr_reader :id, :first_name, :last_name, :date_of_birth
     
     def initialize(options)
-        @id = options["id"] if options["id"]
-        @first_name = options["first_name"]
-        @last_name = options["last_name"]
-        @date_of_birth = options["date_of_birth"]
+        @id = options['id'] if options['id']
+        @first_name = options['first_name']
+        @last_name = options['last_name']
+        @date_of_birth = options['date_of_birth']
     end
 
     def pretty_name()
         return "#{@first_name} #{@last_name}"
+    end
+
+    def save()
+        sql = "INSERT INTO members
+        (
+            first_name,
+            last_name,
+            date_of_birth
+        )
+        VALUES
+        (
+            $1, $2, $3
+        )
+        RETURNING *"
+        values = [@first_name, @last_name, @date_of_birth]
+        member_data = SqlRunner.run(sql, values)
+        @id = member_data.first()['id'].to_i
     end
 end
