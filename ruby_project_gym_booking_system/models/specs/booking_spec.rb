3db7ff42f76c607104631581e99840e63f875dc3
cjscrimmy <craigscrimgeour@yahoo.com>
2020-04-27 13:48:00 +0100

    create test to get member_id from booking

--- ruby_project_gym_booking_system/models/specs/booking_spec.rb
+++ ruby_project_gym_booking_system/models/specs/booking_spec.rb
@@ -1,3 +1,23 @@
 require("minitest/autorun")
 require('minitest/reporters')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative('../booking')
+
+class TestBooking < MiniTest::Test
+
+    def setup()
+        options = {
+            "id" => 1,
+            "member_id" => 36,
+            "lesson_id" => 61
+        }
+
+        @booking = Booking.new(options)
+    end
+
+    def test_member_id()
+        result = @booking.member_id()
+        assert_equal(36, result)
+    end
+end
