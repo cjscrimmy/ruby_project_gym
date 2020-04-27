c1fce88a3f250f77a828be957187a1402e0af021
cjscrimmy <craigscrimgeour@yahoo.com>
2020-04-27 13:09:24 +0100

    add method for updating lesson info. Test using pry and postico. Test passed.

--- ruby_project_gym_booking_system/models/lesson.rb
+++ ruby_project_gym_booking_system/models/lesson.rb
@@ -32,4 +32,23 @@ class Lesson
         lesson_data = SqlRunner.run(sql, values)
         @id = lesson_data.first()['id'].to_i
     end
+
+    def update()
+        sql = "UPDATE lessons
+        SET
+        (
+            lesson_name,
+            lesson_type,
+            day_of_lesson,
+            time_lesson_is_on,
+            week_number_lesson_is_on
+        )= 
+        (
+            $1, $2, $3, $4, $5
+        )
+        WHERE id = $6"
+        values = [@lesson_name, @lesson_type, @day_of_lesson, 
+        @time_lesson_is_on, @week_number_lesson_is_on, @id]
+        SqlRunner.run(sql, values)
+    end
 end
