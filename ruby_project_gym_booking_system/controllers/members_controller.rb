fa1427ecda27c9497e0e6a7354ca913ba2434d4e
cjscrimmy <craigscrimgeour@yahoo.com>
2020-04-28 11:57:00 +0100

    update model files to include appropriate require_relative paths. add view file for showing all a member's info. update members_controller to include the path.

--- ruby_project_gym_booking_system/controllers/members_controller.rb
+++ ruby_project_gym_booking_system/controllers/members_controller.rb
@@ -1,9 +1,14 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative('../models/member')
-also_reload( '..models/*' )
+also_reload( '../models/*' )
 
 get '/members' do
     @members = Member.all()
     erb( :"members/index" )
+end
+
+get '/members/:id' do
+    @member = Member.find_by_id(params['id'].to_i)
+    erb(:"members/show")
 end
--- ruby_project_gym_booking_system/models/member.rb
+++ ruby_project_gym_booking_system/models/member.rb
@@ -1,4 +1,6 @@
 require_relative("../db/sql_runner")
+require_relative("./lesson")
+require_relative("./booking")
 
 class Member
     attr_reader :id
