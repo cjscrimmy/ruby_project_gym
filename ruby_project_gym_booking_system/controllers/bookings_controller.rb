907b4a33f713e2d2c36fb877db70ff094a2496dc
cjscrimmy <craigscrimgeour@yahoo.com>
2020-04-28 19:17:42 +0100

    add method to bookings_controller to show a particular bookings infomation. add a view to display the info.

--- ruby_project_gym_booking_system/controllers/bookings_controller.rb
+++ ruby_project_gym_booking_system/controllers/bookings_controller.rb
@@ -7,3 +7,8 @@ get '/bookings' do
     @bookings = Booking.all()
     erb(:"bookings/index")
 end
+
+get '/bookings/:id' do #show
+    @booking = Booking.find_by_id(params['id'].to_i)
+    erb(:"bookings/show")
+end
