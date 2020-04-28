a7847edc5338237f473e894e756b9f9c36793033
cjscrimmy <craigscrimgeour@yahoo.com>
2020-04-28 13:54:51 +0100

    add methods to members_controller for updating a members details. add view for editing the details. add link to the show view for loading up the edit view

--- ruby_project_gym_booking_system/controllers/members_controller.rb
+++ ruby_project_gym_booking_system/controllers/members_controller.rb
@@ -3,22 +3,37 @@ require( 'sinatra/contrib/all' )
 require_relative('../models/member')
 also_reload( '../models/*' )
 
-get '/members' do
+get '/members' do #index
     @members = Member.all()
     erb( :"members/index" )
 end
 
-get '/members/new' do
+#methods for adding a new member
+get '/members/new' do #new
     erb(:"members/new")
 end
 
-post '/members' do
+post '/members' do #create
     @member = Member.new( params )
     @member.save()
     redirect to '/members'
 end
+#####
 
-get '/members/:id' do
+get '/members/:id' do #show
     @member = Member.find_by_id(params['id'].to_i)
     erb(:"members/show")
 end
+
+#methods for updating a members details
+get '/members/:id/edit' do
+    @member = Member.find_by_id(params['id'].to_i)
+    erb(:"members/edit")
+end
+
+post '/members/:id' do
+    member = Member.new(params)
+    member.update()
+    redirect to "/members/#{params['id']}"
+end
+#####
