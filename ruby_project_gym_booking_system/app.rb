22bb721a939bdf16323241d370f61b848ce6e527
cjscrimmy <craigscrimgeour@yahoo.com>
2020-04-28 10:52:11 +0100

    add first view for members table

--- ruby_project_gym_booking_system/app.rb
+++ ruby_project_gym_booking_system/app.rb
@@ -1,8 +1,8 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative('controllers/members_controller')
-require_relative('controllers/lessons_controller')
-require_relative('controllers/bookings_controller')
+# require_relative('controllers/lessons_controller')
+# require_relative('controllers/bookings_controller')
 
 get '/' do
     erb( :index )
