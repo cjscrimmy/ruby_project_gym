4d1407475f94eae8c0bcce50a5b660ec7bb4ea61
cjscrimmy <craigscrimgeour@yahoo.com>
2020-04-28 15:28:54 +0100

    add functions to lessons_controller for adding a new lesson. add a view for adding the new lesson's details

--- ruby_project_gym_booking_system/controllers/lessons_controller.rb
+++ ruby_project_gym_booking_system/controllers/lessons_controller.rb
@@ -8,6 +8,18 @@ get "/lessons" do #index
     erb(:"lessons/index")
 end
 
+#methods for adding a new lesson
+get "/lessons/new" do #new
+    erb(:"lessons/new")
+end
+
+post "/lessons" do #create
+    @lesson = Lesson.new( params )
+    @lesson.save()
+    redirect to "/lessons"
+end
+#####
+
 get "/lessons/:id" do #show
     @lesson = Lesson.find_by_id(params['id'].to_i)
     erb(:"lessons/show")
