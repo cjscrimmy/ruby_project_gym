4227e61dd4c66c7d344f45d1b4add76c40034a71
cjscrimmy <craigscrimgeour@yahoo.com>
2020-04-28 13:23:34 +0100

    add methods to members_controller for adding a new member. create and add a new view for entering the data.

--- ruby_project_gym_booking_system/controllers/members_controller.rb
+++ ruby_project_gym_booking_system/controllers/members_controller.rb
@@ -8,6 +8,16 @@ get '/members' do
     erb( :"members/index" )
 end
 
+get '/members/new' do
+    erb(:"members/new")
+end
+
+post '/members' do
+    @member = Member.new( params )
+    @member.save()
+    redirect to '/members'
+end
+
 get '/members/:id' do
     @member = Member.find_by_id(params['id'].to_i)
     erb(:"members/show")
