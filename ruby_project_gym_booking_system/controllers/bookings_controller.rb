5e7f2c506bd5b7bfe2325f7f8fc0b72a10432cce
cjscrimmy <craigscrimgeour@yahoo.com>
2020-04-29 09:41:40 +0100

    add methods to bookings_controller to add a new booking. add view to make the new booking.

--- ruby_project_gym_booking_system/controllers/bookings_controller.rb
+++ ruby_project_gym_booking_system/controllers/bookings_controller.rb
@@ -8,6 +8,20 @@ get '/bookings' do
     erb(:"bookings/index")
 end
 
+#methods for adding a new booking
+get '/bookings/new' do #new
+    @members = Member.all()
+    @lessons = Lesson.all()
+    erb(:"bookings/new")
+end
+
+post '/bookings' do #create
+    @booking = Booking.new( params )
+    @booking.save()
+    redirect to '/bookings'
+end
+#####
+
 get '/bookings/:id' do #show
     @booking = Booking.find_by_id(params['id'].to_i)
     erb(:"bookings/show")
